da33f953a6ea9c047c37249f22e946da6effc671
belyy10 <maximgyda10@icloud.com>
2022-09-16 20:45:09 +0300

    done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -2,9 +2,8 @@ const startBtn = document.querySelector('[data-start]');
 const stopBtn = document.querySelector('[data-stop]');
 const bodyRef = document.querySelector('body');
 let timeoutId = null;
-console.log(bodyRef)
-console.log(stopBtn)
-console.log(startBtn)
+startBtn.disabled = false;
+stopBtn.disabled = true;
 
 startBtn.addEventListener('click',changeColor)
 stopBtn.addEventListener('click',stopChangeColor)
@@ -14,10 +13,14 @@ function changeColor(e) {
     timeoutId = setInterval(function(){
         bodyRef.style.backgroundColor = getRandomHexColor();
     },2000);
+    stopBtn.disabled = false;
+    startBtn.disabled = true;
 }
 function stopChangeColor(e) {
     e.preventDefault()
     clearInterval(timeoutId)
+    startBtn.disabled = false;
+    
 }
 function getRandomHexColor() {
     return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
